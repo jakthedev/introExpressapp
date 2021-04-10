4885c180720d675ca5a3d1e8fe1fca0e23d9401b
JAKThaDev <jakthadev@Jds-MacBook-Pro.local>
2021-04-10 16:04:42 -0400

    Started setting up express router

--- server.js
+++ server.js
@@ -11,7 +11,38 @@ const port = 3000;
 const app = express();  
 // This will configure morgan to logg the develpment verison  
 //this will print additional information to the screen
-app.use(morgan('dev'));
+app.use(morgan('dev')); 
+app.use(express.json());
+
+// So any HTTP request to this path will trigger this method. 
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200; 
+    res.setHeader('Content-Type', 'text/plain'); 
+    next();
+}); 
+
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+}); 
+
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`)
+});  
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403; 
+    res.end('PUT operation not supported on /campsites');
+}); 
+
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');  
+
+});
+
+// creating HTTP methods for different endpoints. 
+ app.get('/campsites/:campsiteId', (req, res) => {
+     
+ })
 
 // now we are going to setup express to server file files from the public folder 
 app.use(express.static(__dirname + '/public'));
