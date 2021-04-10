0628913d6c6f29cbfd2ca8838fcaa22d3c84636f
JAKThaDev <jdakemp@gmail.com>
2021-04-10 19:04:10 -0400

    implemented REST API end points

--- server.js
+++ server.js
@@ -41,8 +41,24 @@ app.delete('/campsites', (req, res) => {
 
 // creating HTTP methods for different endpoints. 
  app.get('/campsites/:campsiteId', (req, res) => {
-     
- })
+     res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+ }); 
+
+ app.post('/campsites/:campsiteId', (req, res) => {
+     // stage a post request message will use later. 
+     res.statusCode = 403; 
+     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`)
+ }) 
+
+ app.put('/campsites/:campsiteId', (req, res) => {
+     res.write(`Updating the campsite: ${req.params.campsiteId}\n`); 
+     res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`); 
+ }); 
+
+ app.delete('/campsites/:campsiteId', (req, res) => {
+     res.end(`Delecting campsite: ${req.params.campsiteId}`);
+ });
 
 // now we are going to setup express to server file files from the public folder 
 app.use(express.static(__dirname + '/public'));
