fe686d5a103344fdb08959f286494b9a4231a63f
JAKThaDev <jakthadev@Jds-MacBook-Pro.local>
2021-04-10 12:28:39 -0400

    starting to create express server

---
+++ server.js
@@ -0,0 +1,7 @@
+// even tho express is not a core module since it has been installed to node module we dont need 
+// give it a file name we dont have to tell node to look into node modules for express 
+// it will do that automatically
+const express = require('express'); 
+
+const hostname = 'localhost'; 
+const port = 3000;
